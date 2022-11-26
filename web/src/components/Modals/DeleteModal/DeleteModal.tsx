31819be6d872e644eb09dbfce09b74fd6b063b59
Pedro Lima <contact.pedrolima@gmail.com>
2022-11-26 12:04:35 -0300

    modals compatibility update

--- web/src/components/Modals/DeleteModal/DeleteModal.tsx
+++ web/src/components/Modals/DeleteModal/DeleteModal.tsx
@@ -67,7 +67,7 @@ export function DeleteModal() {
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-80" />
-      <Dialog.Content className="flex flex-col flex-1 max-h-auto max-w-auto items-center justify-between text-ctp-text bg-ctp-crust rounded-xl fixed top-1/2 left-1/2 w-1/2 h-1/2 p-4 translate-x-[-50%] translate-y-[-50%]">
+      <Dialog.Content className="flex flex-col flex-1 max-h-auto max-w-auto items-center justify-between text-ctp-text bg-ctp-crust rounded-xl fixed top-1/2 left-1/2 w-auto h-auto gap-6 p-6 translate-x-[-50%] translate-y-[-50%]">
         <Dialog.Title className="text-2xl text-ctp-text font-bold">
           <p>Delete tasklist {tasklist}?</p>
         </Dialog.Title>
