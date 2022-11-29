7ec39fd1a7da5075611082fb68dbc4afd1d5fe99
Pedro Lima <contact.pedrolima@gmail.com>
2022-11-29 11:02:03 -0300

    mobile version update

--- web/src/components/TaskContainer/TaskContainer.tsx
+++ web/src/components/TaskContainer/TaskContainer.tsx
@@ -68,7 +68,7 @@ export function TaskContainer({ isSideBarOpen }: TaskContainerProps) {
     <main className="flex flex-col w-3/4 h-screen items-center justify-between py-4">
       <TaskContainerHeader />
       <Loading active={isLoading} type="balls" size={60} />
-      <div className="md:w-11/12 m-2 md:max-h-[80%] max-h-[70%] rounded-lg h-full flex p-2 flex-col gap-2 items-center overflow-y-scroll scrollbar border-2 border-ctp-overlay0">
+      <div className="w-11/12 m-2 md:max-h-[80%] max-h-[70%] rounded-lg h-full flex p-2 flex-col gap-2 items-center overflow-y-scroll scrollbar border-2 border-ctp-overlay0">
         {tasklist !== "" ? (
           tasks.length > 0 && tasks !== undefined ? (
             tasks.map((task: TaskProps, index: any) =>
